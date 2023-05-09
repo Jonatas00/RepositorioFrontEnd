b6c0b5f78a5b8bf731abc7525cfe6270d690cd2a
Jonatas <djonatas31@gmail.com>
2023-05-08 22:11:42 -0300

    First Commit

---
+++ TreinosJS/01/script.js
@@ -0,0 +1,6 @@
+function mudarCor() {
+    let fundo = document.querySelector("body")
+    let button = document.querySelector("button")
+    fundo.classList.toggle("darkThemeBody")
+    button.classList.toggle("darkThemeButton")
+}
