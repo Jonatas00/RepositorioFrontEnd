4fe7c1bb29d2a67bf7e6e8b304b285fd5b8f23b4
Jonatas <djonatas31@gmail.com>
2023-05-08 22:27:48 -0300

    update var name

--- TreinosJS/01FirstDOMTest/script.js
+++ TreinosJS/01FirstDOMTest/script.js
@@ -1,6 +1,6 @@
 function mudarCor() {
     let fundo = document.querySelector("body")
-    let button = document.querySelector("button")
+    let botao = document.querySelector("button")
     fundo.classList.toggle("darkThemeBody")
     button.classList.toggle("darkThemeButton")
 }
